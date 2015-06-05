b96c9316fd0b7017315c06842a770f6fa8ea7d46
Nan Wang <nantas@gmail.com>
2015-06-05 12:27:54 +0800

    #15 added native-modules list in package.json, run list in test.

--- test/native-modules.js
+++ test/native-modules.js
@@ -1,8 +1,14 @@
+var Fs = require('fire-fs');
 
 describe('NativeModules', function() {
-  it('should require native module successfully', function( done ) {
-      var fontmng = require('font-manager');
-      expect(fontmng).to.be.ok;
-      done();
+  var list = JSON.parse(Fs.readFileSync('package.json'))['native-modules'];
+  list.forEach(function(item) {
+    describe('module ' + item, function() {
+      it('should require native module successfully', function( done ) {
+        var npmModule = require(item);
+        expect(npmModule).to.be.ok;
+        done();
+  	  });    
+    });
   });
 });
